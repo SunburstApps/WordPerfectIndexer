bc5ea73088788c1ed7e85a5dfed5821f4fb4632a
William Kent <wjk011@gmail.com>
2017-04-02 13:45:03 -0400

    Fix kooky formatting in WordPerfectIndexer.cpp

--- WordPerfectIndexer/WordPerfectIndexer.cpp
+++ WordPerfectIndexer/WordPerfectIndexer.cpp
@@ -1,43 +1,30 @@
-// WordPerfectIndexer.cpp : Implementation of DLL Exports.
-
-
 #include "stdafx.h"
 #include "resource.h"
 #include "WordPerfectIndexer_i.h"
 #include "dllmain.h"
 
-
 using namespace ATL;
 
-// Used to determine whether the DLL can be unloaded by OLE.
 STDAPI DllCanUnloadNow(void)
 {
-			return _AtlModule.DllCanUnloadNow();
-	}
-
-// Returns a class factory to create an object of the requested type.
-_Check_return_
-STDAPI DllGetClassObject(_In_ REFCLSID rclsid, _In_ REFIID riid, _Outptr_ LPVOID* ppv)
-{
-		return _AtlModule.DllGetClassObject(rclsid, riid, ppv);
+	return _AtlModule.DllCanUnloadNow();
 }
 
-// DllRegisterServer - Adds entries to the system registry.
+_Check_return_ STDAPI DllGetClassObject(_In_ REFCLSID rclsid, _In_ REFIID riid, _Outptr_ LPVOID* ppv)
+{
+	return _AtlModule.DllGetClassObject(rclsid, riid, ppv);
+}
+
 STDAPI DllRegisterServer(void)
 {
-	// registers object, typelib and all interfaces in typelib
-	HRESULT hr = _AtlModule.DllRegisterServer();
-		return hr;
+	return _AtlModule.DllRegisterServer();
 }
 
-// DllUnregisterServer - Removes entries from the system registry.
 STDAPI DllUnregisterServer(void)
 {
-	HRESULT hr = _AtlModule.DllUnregisterServer();
-		return hr;
+	return _AtlModule.DllUnregisterServer();
 }
 
-// DllInstall - Adds/Removes entries to the system registry per user per machine.
 STDAPI DllInstall(BOOL bInstall, _In_opt_  LPCWSTR pszCmdLine)
 {
 	HRESULT hr = E_FAIL;
@@ -66,5 +53,3 @@ STDAPI DllInstall(BOOL bInstall, _In_opt_  LPCWSTR pszCmdLine)
 
 	return hr;
 }
-
-
