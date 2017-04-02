5fb230e3e54230de2354ab141806eed773494c03
William Kent <wjk011@gmail.com>
2017-04-02 19:15:10 -0400

    Fix librevenge dllimport functionality

--- librevenge/inc/librevenge-stream/librevenge-stream-api.h
+++ librevenge/inc/librevenge-stream/librevenge-stream-api.h
@@ -21,18 +21,10 @@
 #ifndef INCLUDED_LIBREVENGE_STREAM_LIBREVENGE_STREAM_API_H
 #define INCLUDED_LIBREVENGE_STREAM_LIBREVENGE_STREAM_API_H
 
-#ifdef DLL_EXPORT
 #ifdef LIBREVENGE_STREAM_BUILD
 #define REVENGE_STREAM_API __declspec(dllexport)
 #else
 #define REVENGE_STREAM_API __declspec(dllimport)
-#endif
-#else // !DLL_EXPORT
-#ifdef LIBREVENGE_STREAM_VISIBILITY
-#define REVENGE_STREAM_API __attribute__((visibility("default")))
-#else
-#define REVENGE_STREAM_API
-#endif
 #endif
 
 #endif /* INCLUDED_LIBREVENGE_STREAM_LIBREVENGE_STREAM_API_H */
--- librevenge/inc/librevenge/librevenge-api.h
+++ librevenge/inc/librevenge/librevenge-api.h
@@ -21,18 +21,10 @@
 #ifndef INCLUDED_LIBREVENGE_LIBREVENGE_API_H
 #define INCLUDED_LIBREVENGE_LIBREVENGE_API_H
 
-#ifdef DLL_EXPORT
 #ifdef LIBREVENGE_BUILD
 #define REVENGE_API __declspec(dllexport)
 #else
 #define REVENGE_API __declspec(dllimport)
-#endif
-#else // !DLL_EXPORT
-#ifdef LIBREVENGE_VISIBILITY
-#define REVENGE_API __attribute__((visibility("default")))
-#else
-#define REVENGE_API
-#endif
 #endif
 
 #ifdef __GNUC__
