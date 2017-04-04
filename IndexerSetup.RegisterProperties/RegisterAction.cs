58eeed137f5ff8ca5edf7dde0204a35c073b0edb
William Kent <wjk011@gmail.com>
2017-04-03 21:17:11 -0400

    Remove unneeded custom action code

--- IndexerSetup.RegisterProperties/RegisterAction.cs
+++ IndexerSetup.RegisterProperties/RegisterAction.cs
@@ -19,21 +19,7 @@ namespace IndexerSetup.RegisterProperties
             else return ActionResult.Failure;
         }
 
-        [CustomAction]
-        public static ActionResult UnregisterPropDescFile(Session session)
-        {
-            session.Log("Unregistering property description file...");
-
-            string propdescPath = Path.Combine(session.GetTargetPath("INSTALLDIR"), "WordPerfectIndexer.propdesc");
-            bool registerSucceeded = PSUnregisterPropertySchema(propdescPath);
-
-            if (registerSucceeded) return ActionResult.Success;
-            else return ActionResult.Failure;
-        }
-
         [DllImport("propsys.dll", CharSet = CharSet.Unicode)]
         public static extern bool PSRegisterPropertySchema(string propdescPath);
-        [DllImport("propsys.dll", CharSet = CharSet.Unicode)]
-        public static extern bool PSUnregisterPropertySchema(string propdescPath);
     }
 }
