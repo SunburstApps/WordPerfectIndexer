98e45be066c44974dbcf3916bad06717fd3a98c1
William Kent <wjk011@gmail.com>
2017-04-04 19:29:15 -0400

    Call SHChangeNotify after modifying the file associations

--- IndexerSetup.RegisterProperties/RegisterAction.cs
+++ IndexerSetup.RegisterProperties/RegisterAction.cs
@@ -14,12 +14,16 @@ namespace IndexerSetup.RegisterProperties
 
             string propdescPath = Path.Combine(session.GetTargetPath("INSTALLFOLDER"), "WordPerfectIndexer.propdesc");
             bool registerSucceeded = PSRegisterPropertySchema(propdescPath);
+            if (!registerSucceeded) return ActionResult.Failure;
 
-            if (registerSucceeded) return ActionResult.Success;
-            else return ActionResult.Failure;
+            SHChangeNotify(0x08000000, 0, IntPtr.Zero, IntPtr.Zero);
+            return ActionResult.Success;
         }
 
         [DllImport("propsys.dll", CharSet = CharSet.Unicode)]
         public static extern bool PSRegisterPropertySchema(string propdescPath);
+
+        [DllImport("shell32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern void SHChangeNotify(long eventId, uint flags, IntPtr data1, IntPtr data2);
     }
 }
