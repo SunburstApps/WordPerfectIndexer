bb7610caa09a75a7f4b0cded734d711e73078268
William Kent <wjk011@gmail.com>
2017-04-05 18:26:30 -0400

    Add LogMessages.mc to WordPerfectIndexer project
    This file defines messages that WordPerfectIndexer will log
    to the Event Viewer to aid in debugging.

---
+++ WordPerfectIndexer/LogMessages.h
@@ -0,0 +1,125 @@
+// Begin header section.
+// Begin event category section.
+//
+//  Values are 32 bit values laid out as follows:
+//
+//   3 3 2 2 2 2 2 2 2 2 2 2 1 1 1 1 1 1 1 1 1 1
+//   1 0 9 8 7 6 5 4 3 2 1 0 9 8 7 6 5 4 3 2 1 0 9 8 7 6 5 4 3 2 1 0
+//  +---+-+-+-----------------------+-------------------------------+
+//  |Sev|C|R|     Facility          |               Code            |
+//  +---+-+-+-----------------------+-------------------------------+
+//
+//  where
+//
+//      Sev - is the severity code
+//
+//          00 - Success
+//          01 - Informational
+//          10 - Warning
+//          11 - Error
+//
+//      C - is the Customer code flag
+//
+//      R - is a reserved bit
+//
+//      Facility - is the facility code
+//
+//      Code - is the facility's status code
+//
+//
+// Define the facility codes
+//
+#define FACILITY_METADATA_FILTER         0x0
+
+
+//
+// Define the severity codes
+//
+#define STATUS_SEVERITY_WARNING          0x2
+#define STATUS_SEVERITY_SUCCESS          0x0
+#define STATUS_SEVERITY_INFORMATIONAL    0x1
+#define STATUS_SEVERITY_ERROR            0x3
+
+
+//
+// MessageId: TEXT_EXTRACTION_CATEGORY
+//
+// MessageText:
+//
+// Text extraction using libwpd
+//
+#define TEXT_EXTRACTION_CATEGORY         ((WORD)0x00000001L)
+
+//
+// MessageId: CHUNK_CATEGORY
+//
+// MessageText:
+//
+// Report metadata to Windows
+//
+#define CHUNK_CATEGORY                   ((WORD)0x00000002L)
+
+// Begin event definition section.
+//
+// MessageId: MSG_GETTEMPPATH_FAILED
+//
+// MessageText:
+//
+// GetTempPath() call failed (error %1)
+//
+#define MSG_GETTEMPPATH_FAILED           ((DWORD)0xC0000100L)
+
+//
+// MessageId: MSG_GETTEMPFILENAME_FAILED
+//
+// MessageText:
+//
+// GetTempFileName() call failed (error %1)
+//
+#define MSG_GETTEMPFILENAME_FAILED       ((DWORD)0xC0000101L)
+
+//
+// MessageId: MSG_FOPEN_FAILED
+//
+// MessageText:
+//
+// Could not open file %1!s! (errno %2)
+//
+#define MSG_FOPEN_FAILED                 ((DWORD)0xC0000102L)
+
+//
+// MessageId: MSG_STREAM_READ_ERROR
+//
+// MessageText:
+//
+// Could not read from stream (HRESULT %1)
+//
+#define MSG_STREAM_READ_ERROR            ((DWORD)0xC0000103L)
+
+//
+// MessageId: MSG_WPD_DOC_CORRUPT
+//
+// MessageText:
+//
+// Could not read parse WordPerfect document at path %1!s!
+//
+#define MSG_WPD_DOC_CORRUPT              ((DWORD)0xC0000104L)
+
+//
+// MessageId: MSG_BEGIN_IMPORT
+//
+// MessageText:
+//
+// Beginning import of WordPerfect document
+//
+#define MSG_BEGIN_IMPORT                 ((DWORD)0x40000105L)
+
+//
+// MessageId: MSG_END_IMPORT
+//
+// MessageText:
+//
+// WordPerfect document import completed successfully
+//
+#define MSG_END_IMPORT                   ((DWORD)0x40000106L)
+
---
+++ WordPerfectIndexer/LogMessages.mc
@@ -0,0 +1,89 @@
+;// Begin header section.
+
+SeverityNames=(
+	Success=0x0:STATUS_SEVERITY_SUCCESS
+	Informational=0x1:STATUS_SEVERITY_INFORMATIONAL
+	Warning=0x2:STATUS_SEVERITY_WARNING
+	Error=0x3:STATUS_SEVERITY_ERROR
+)
+
+FacilityNames=(
+	Filter=0x0:FACILITY_METADATA_FILTER
+)
+
+LanguageNames=(English=0x409:MSG00409)
+
+;// Begin event category section.
+
+MessageIdTypedef=WORD
+
+MessageId=0x1
+SymbolicName=TEXT_EXTRACTION_CATEGORY
+Language=English
+Text extraction using libwpd
+.
+
+MessageId=0x2
+SymbolicName=CHUNK_CATEGORY
+Language=English
+Report metadata to Windows
+.
+
+;// Begin event definition section.
+
+MessageIdTypedef=DWORD
+
+MessageId=0x100
+Severity=Error
+Facility=Filter
+SymbolicName=MSG_GETTEMPPATH_FAILED
+Language=English
+GetTempPath() call failed (error %1)
+.
+
+MessageId=0x101
+Severity=Error
+Facility=Filter
+SymbolicName=MSG_GETTEMPFILENAME_FAILED
+Language=English
+GetTempFileName() call failed (error %1)
+.
+
+MessageId=0x102
+Severity=Error
+Facility=Filter
+SymbolicName=MSG_FOPEN_FAILED
+Language=English
+Could not open file %1!s! (errno %2)
+.
+
+MessageId=0x103
+Severity=Error
+Facility=Filter
+SymbolicName=MSG_STREAM_READ_ERROR
+Language=English
+Could not read from stream (HRESULT %1)
+.
+
+MessageId=0x104
+Severity=Error
+Facility=Filter
+SymbolicName=MSG_WPD_DOC_CORRUPT
+Language=English
+Could not read parse WordPerfect document at path %1!s!
+.
+
+MessageId=0x105
+Severity=Informational
+Facility=Filter
+SymbolicName=MSG_BEGIN_IMPORT
+Language=English
+Beginning import of WordPerfect document
+.
+
+MessageId=0x106
+Severity=Informational
+SymbolicName=MSG_END_IMPORT
+Language=English
+WordPerfect document import completed successfully
+.
