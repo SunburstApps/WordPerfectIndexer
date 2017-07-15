58bfbecc97d55c8de29e3057cf8d88a54f228b5d
William Kent <wjk011@gmail.com>
2017-07-15 17:16:35 -0400

    Remove dead code

--- WordPerfectIndexer/CWordPerfectFilter.cpp
+++ WordPerfectIndexer/CWordPerfectFilter.cpp
@@ -10,31 +10,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-namespace /* anonymous */
-{
-	PWSTR StrDupA2W(PCSTR ascii)
-	{
-		size_t asciilen = strlen(ascii);
-		size_t widelen = MultiByteToWideChar(CP_ACP, 0, ascii, asciilen, nullptr, 0);
-
-		PWSTR wide = (PWSTR)malloc(widelen * sizeof(wchar_t));
-		MultiByteToWideChar(CP_ACP, 0, ascii, asciilen, wide, widelen);
-		return wide;
-	}
-
-	bool ParseGUID(GUID& guid, LPCWSTR string)
-	{
-		const int expected_fields = 11;
-#pragma warning(suppress: 4996) // Don't warn about CRT security deprecations
-		int num_fields_parsed = swscanf(string, L"%08X-%04hX-%04hX-%02hhX%02hhX-%02hhX%02hhX%02hhX%02hhX%02hhX%02hhX",
-			&guid.Data1, &guid.Data2, &guid.Data3,
-			&guid.Data4[0], &guid.Data4[1], &guid.Data4[2], &guid.Data4[3],
-			&guid.Data4[4], &guid.Data4[5], &guid.Data4[6], &guid.Data4[7]);
-
-		return num_fields_parsed == expected_fields;
-	}
-}
-
 class CWordPerfectFilter::Private
 {
 public:
